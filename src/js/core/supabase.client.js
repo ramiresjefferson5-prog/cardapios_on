a82a70c464116ec7fff477a2ac8d3a903f99913a
ramiresjefferson5-prog <ramiresjefferson5@gmail.com>
2026-05-01 19:04:19 -0300

    Delete src directory

--- src/js/core/supabase.client.js
+++
@@ -1,4 +0,0 @@
-const supabaseClient = window.supabase.createClient(
-    'https://lqfwpfaqcnfsybxchmtg.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZndwZmFxY25mc3lieGNobXRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2ODk5MjIsImV4cCI6MjA5MDI2NTkyMn0.XMO-YQIzULWK2yMFoqdBVP-wYPA7P84sp4QnsuS0hks'
-);
--- src/js/core/viewport.js
+++
@@ -1,6 +0,0 @@
-function setAppHeight() {
-            document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
-        }
-        window.addEventListener('resize', setAppHeight);
-        window.addEventListener('orientationchange', setAppHeight);
-        setAppHeight();
